f2c57983cf42b3ad88e428bafc942ca4709ec5d1
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2025-01-24 13:16:05 +0500

    cloudinary setup complete

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,44 +1,35 @@
 import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
 import { ENV } from "../constant.js";
 import 'dotenv/config'
 
-(async function() {
+// Configuration
 
-    // Configuration
-    cloudinary.config({ 
-        cloud_name: ENV.CLOUD-NAME, 
-        api_key: '', 
-        api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
-    });
-    
-    // Upload an image
-     const uploadResult = await cloudinary.uploader
-       .upload(
-           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-               public_id: 'shoes',
-           }
-       )
-       .catch((error) => {
-           console.log(error);
-       });
-    
-    console.log(uploadResult);
-    
-    // Optimize delivery by resizing and applying auto-format and auto-quality
-    const optimizeUrl = cloudinary.url('shoes', {
-        fetch_format: 'auto',
-        quality: 'auto'
-    });
-    
-    console.log(optimizeUrl);
-    
-    // Transform the image: auto-crop to square aspect_ratio
-    const autoCropUrl = cloudinary.url('shoes', {
-        crop: 'auto',
-        gravity: 'auto',
-        width: 500,
-        height: 500,
-    });
-    
-    console.log(autoCropUrl);    
-})();
+cloudinary.config({
+    cloud_name: ENV.CLOUDINARY_CLOUD_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (LocalFilePath) => {
+    try {
+        if (!LocalFilePath) return null;
+        const response = await cloudinary.uploader.upload(LocalFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        console.log("file uploaded successfully", response.url);
+        fs.unlinkSync(LocalFilePath);
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(LocalFilePath); // remove the locally saved temporary file as the upload operation got failed
+        return null
+        console.log(error);
+
+    }
+}
+
+export {
+    uploadOnCloudinary
+}
